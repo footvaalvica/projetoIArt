970385170c395be8b5de51d1c6e348f4b04fb317
Mateus Pinho <mateusleitepinho@gmail.com>
2022-05-29 15:22:51 +0100

    changed order of file

--- takuzu.py
+++ takuzu.py
@@ -6,7 +6,6 @@
 # 99282 Mateus Pinho
 # 99238 Inês Ji
 
-from cgi import test
 import sys
 import numpy as np
 from search import (
@@ -18,20 +17,6 @@ from search import (
     greedy_search,
     recursive_best_first_search,
 )
-
-class TakuzuState:
-    state_id = 0
-
-    def __init__(self, board):
-        self.board = board
-        self.id = TakuzuState.state_id
-        TakuzuState.state_id += 1
-
-    def __lt__(self, other):
-        return self.id < other.id
-
-    # TODO: outros metodos da classe
-
 
 class Board:
     """Representação interna de um tabuleiro de Takuzu."""
@@ -54,14 +39,32 @@ class Board:
     def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
         """Devolve os valores imediatamente abaixo e acima,
         respectivamente."""
-        #TODO
-        pass
+        n = self.board.shape
+        if n[0] > row+1:
+            nbelow = self.board[row+1, col]
+        else:
+            nbelow = None
+        if row+1 != 1:
+            nabove = self.board[row-1, col]
+        else:
+            nabove = None
+            
+        return (nbelow, nabove)
 
     def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
-        #TODO
-        pass
+        n = self.board.shape
+        if n[1] > col+1:
+            nright = self.board[row, col+1]
+        else:
+            nright = None
+        if col+1 != 1:
+            nleft = self.board[row, col-1]
+        else:
+            nleft = None
+        
+        return (nleft, nright)
 
     def transpose(self):
         """Devolve a transposição do tabuleiro."""
@@ -90,6 +93,19 @@ class Board:
 
     # TODO: outros metodos da classe
 
+class TakuzuState:
+    state_id = 0
+
+    def __init__(self, board: Board):
+        self.board = board
+        self.id = TakuzuState.state_id
+        TakuzuState.state_id += 1
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    # TODO: outros metodos da classe
+
 class Takuzu(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
@@ -115,7 +131,9 @@ class Takuzu(Problem):
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas com uma sequência de números adjacentes."""
         # TODO
-        pass
+        board = state.board
+        for row in board:
+            bla bla bla
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
@@ -135,6 +153,7 @@ class Test:
 2 2 0 2
 2 0 2 2
 1 1 2 0
+
 (None, 2)
 (2, None)
 (0, 1)
@@ -144,6 +163,7 @@ class Test:
 2 2 0 2
 2 0 2 2
 1 1 2 0
+
 2
 1
 9"""
@@ -152,6 +172,7 @@ class Test:
 2 2 0 2
 2 0 2 2
 1 1 2 0
+
 Is goal? True
 Solution:
 0 1 1 0
@@ -168,10 +189,15 @@ Solution:
 
     @staticmethod
     def prGreen(prt):
-        print("\033[92m{}\033[00m" .format(prt))
+        print("\033[92m{}\033[00m".format(prt))
 
+    @staticmethod
     def prCyan(prt):
-        print("\033[96m{}\033[00m" .format(prt))
+        print("\033[96m{}\033[00m".format(prt))
+
+    @staticmethod
+    def prRed(prt):
+        print("\033[91m{}\033[00m".format(prt))
 
     def __init__(self, board: Board, debug: bool = True):
         """Construtor da classe Test."""
@@ -189,16 +215,19 @@ Solution:
     
     @staticmethod
     def test1(board: Board):
-        testOutput = ""
-        testOutput + str("Initial:\n" + str(board))
+        testOutput = str("Initial:\n" + str(board)) + "\n"
         # Imprimir valores adjacentes
-        testOutput + str(board.adjacent_vertical_numbers(3, 3))
-        testOutput + str(board.adjacent_horizontal_numbers(3, 3))
-        testOutput + str(board.adjacent_vertical_numbers(1, 1))
-        testOutput + str(board.adjacent_horizontal_numbers(1, 1))
+        testOutput += str(board.adjacent_vertical_numbers(3, 3)) + "\n"
+        testOutput += str(board.adjacent_horizontal_numbers(3, 3)) + "\n"
+        testOutput += str(board.adjacent_vertical_numbers(1, 1)) + "\n"
+        testOutput += str(board.adjacent_horizontal_numbers(1, 1))
 
         if testOutput == Test.test1out:
-            Test.prGreen("Nice!\n")
+            Test.prGreen("Nice!")
+        else:
+            Test.prRed("Wrong!")
+            Test.prCyan(Test.test1out)
+            print(testOutput)
 
     @staticmethod
     def test1debug(board: Board):
@@ -321,7 +350,7 @@ if __name__ == "__main__":
     board = Board.parse_instance_from_stdin()
 
     #isto é para correr os testes
-    Test(board)
+    # # Test(board)
 
     # isto corre os testes sem debug (diz só se está certo ou nao)
-    # # Test(False)
+    Test(board, False)
